b97273e5a58e08c57f2985eba42b9c1b365aa8e9
Dean Polimac <deanpolimac@yahoo.com>
2025-11-21 02:46:34 +0100

    Update ApplicationTest
    The application test now passes the console in the Thread instead of the TaskList, as previously done.

--- java/src/test/java/com/ortecfinance/tasklist/ApplicationTest.java
+++ java/src/test/java/com/ortecfinance/tasklist/ApplicationTest.java
@@ -21,8 +21,8 @@ public final class ApplicationTest {
     public ApplicationTest() throws IOException {
         BufferedReader in = new BufferedReader(new InputStreamReader(new PipedInputStream(inStream)));
         PrintWriter out = new PrintWriter(new PipedOutputStream(outStream), true);
-        TaskList taskList = new TaskList(in, out);
-        applicationThread = new Thread(taskList);
+        Console console = new Console(in, out);
+        applicationThread = new Thread(console);
     }
 
     @BeforeEach
